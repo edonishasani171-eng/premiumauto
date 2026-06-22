9070eb70ca7171c7c19f5a8656659b25cb9f5833
edonishasani171-eng <edonishasani171@gmail.com>
2026-06-22 21:26:27 +0200

    Update database connection details for Clever Cloud

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,9 @@
 <?php
 // Look for Clever Cloud environment variables on Render, fall back to local XAMPP if not found
-$host = getenv('DB_HOST')     ?: 'localhost';
-$username = getenv('DB_USER')     ?: 'root';
-$password = getenv('DB_PASSWORD') ?: ''; // Default XAMPP password is empty
-$dbname = getenv('DB_NAME')     ?: 'carorder_db';
+$host = getenv('DB_HOST')     ?: 'b7wqnxtrqrmjsplfibrn-mysql.services.clever-cloud.com';
+$username = getenv('DB_USER')     ?: 'uvibbujjmdiva7cq';
+$password = getenv('DB_PASSWORD') ?: '95nGnn40Cj3F1Q2ov2yO'; // Default XAMPP password is empty
+$dbname = getenv('DB_NAME')     ?: 'b7wqnxtrqrmjsplfibrn';
 $port = getenv('DB_PORT')     ?: '3306';
 
 // Create connection
