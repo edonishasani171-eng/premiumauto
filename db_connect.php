061995cdc0e6e936b187d29b48a349aff0bde9c5
Edonis Hasani <edonishasani171@gmail.com>
2026-06-23 15:07:51 +0200

    fixed ini_set got removed

--- db_connect.php
+++ db_connect.php
@@ -13,8 +13,6 @@ $conn = new mysqli($host, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// Store sessions in database instead of filesystem
-ini_set('session.save_handler', 'user');
 
 class DBSessionHandler implements SessionHandlerInterface {
     private $conn;
